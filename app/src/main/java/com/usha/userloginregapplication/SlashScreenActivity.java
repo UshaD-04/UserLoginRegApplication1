51e0dd4fb1bd6b04178d017d6479c47103d0fbea
UshaD-04 <usha.dalavi04@gmail.com>
2021-07-06 14:18:39 +0530

    splashcreen added

--- app/src/main/java/com/usha/userloginregapplication/SlashScreenActivity.java
+++ app/src/main/java/com/usha/userloginregapplication/SlashScreenActivity.java
@@ -2,7 +2,10 @@ package com.usha.userloginregapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SlashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SlashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+                finish();
+
+            }
+        }, 3000);
+
     }
 }
